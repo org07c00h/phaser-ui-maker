eb96a68fb0368b606edbc96f0c17ff4ee0f3c23e
Motoko Kusanagi <42@ginko.moe>
2016-11-05 10:57:49 -0400

    Added scaling support

---
+++ js/Game.js
@@ -0,0 +1,271 @@
+BasicGame.Game = function (game) {
+
+	//	When a State is added to Phaser it automatically has the following properties set on it, even if they already exist:
+
+    this.game;		//	a reference to the currently running game
+    this.add;		//	used to add sprites, text, groups, etc
+    this.camera;	//	a reference to the game camera
+    this.cache;		//	the game cache
+    this.input;		//	the global input manager (you can access this.input.keyboard, this.input.mouse, as well from it)
+    this.load;		//	for preloading assets
+    this.math;		//	lots of useful common math operations
+    this.sound;		//	the sound manager - add a sound, play one, set-up markers, etc
+    this.stage;		//	the game stage
+    this.time;		//	the clock
+    this.tweens;	//	the tween manager
+    this.world;		//	the game world
+    this.particles;	//	the particle manager
+    this.physics;	//	the physics manager
+    this.rnd;		//	the repeatable random number generator
+    this.buttons;
+    this.ui;
+    this.interface;
+    //	You can use any of these from any function within this State.
+    //	But do consider them as being 'reserved words', i.e. don't create a property for your own game called "world" or you'll over-write the world reference.
+
+};
+
+BasicGame.Game.prototype = {
+
+	create: function () {
+        this.buttons = game.add.group();
+        this.ui = game.add.group();
+        this.interface = game.add.group();
+        // initialize square for scaling
+        for (var key in scaleUI) {
+            let square = game.add.image(20, 20, 'square');
+            square.alpha = 0;
+            this.interface.add(square);
+            scaleUI[key] = square;
+            scaleUI[key].inputEnabled = false;
+            square.key = key;   // js sucks
+            scaleUI[key].events.onInputOver.add((square) => {
+                canvas.style.cursor = square.key + "-resize";
+            }, this);
+            scaleUI[key].events.onInputOut.add(() => {
+                canvas.style.cursor = "default";
+            });
+        }
+        this.configureScaleButtons();
+        centerPointer = game.add.image(120, 20, 'centerPointer');
+        let images = game.cache._cacheMap[Phaser.Cache.IMAGE];
+        let frames = images.atlas.frameData._frames;
+        let y = 20;
+        for (let i = 0; i < frames.length; i++) {
+            let frame =  images.atlas.frameData._frames[i];
+            let image = game.add.image(game.world.centerX - 100, y, "atlas", frame.name);
+            image.inputEnabled = true;
+            image.events.onInputDown.add((image) => {
+                this.ui.alpha = 1;
+                this.toggleAtlasInput();
+                let img = game.add.sprite(game.world.centerX, game.world.centerY, "atlas", image._frame.name);
+                img.inputEnabled = true;
+                img.input.enableDrag(true);
+                img.options = {
+                    type: "sprite",
+                    height: img.height,
+                    width: img.width
+                };
+                img.events.onInputDown.add((image) => {
+                    updateOptions(image);
+                    this.interface.alpha = 1;
+                }, this);
+                img.events.onDragUpdate.add((image) => {
+                    updateUIPosition(image);
+                }, this)
+                img.events.onDragStop.add((image) => {
+                    canvas.style.cursor = "default";
+                    updateOptions(image);
+                }, this);
+                img.events.onDragStart.add(()=> {
+                    canvas.style.cursor = "move";
+                }, this)
+                // img.events.on
+                this.ui.add(img);
+                this.buttons.alpha = 0;
+            }, this);
+            y += image.height + 10;
+            this.buttons.add(image);
+        }
+		//	Honestly, just about anything could go here. It's YOUR game after all. Eat your heart out!
+
+	},
+
+	update: function () {
+		//	Honestly, just about anything could go here. It's YOUR game after all. Eat your heart out!
+        if (showMenu) {
+            this.toggleAtlasInput();
+            disableScaleUI();
+            this.buttons.alpha = 1;
+            this.ui.alpha = 0;
+            this.interface.alpha = 0;
+            showMenu = false;
+        }
+	},
+
+	quitGame: function (pointer) {
+
+		//	Here you should destroy anything you no longer need.
+		//	Stop music, delete sprites, purge caches, free resources, all that good stuff.
+
+		//	Then let's go back to the main menu.
+		this.state.start('MainMenu');
+
+	}
+};
+function updateOptions(image) {
+    currentImage = image;
+    imageOptions.style.display = "block";
+    document.getElementById("type").value = image.options.type;
+    document.getElementById("pos_x").value = image.x;
+    document.getElementById("pos_y").value = image.y;
+    document.getElementById("scale_x").value = image.scale.x;
+    document.getElementById("scale_y").value = image.scale.y;
+    document.getElementById("anchor_x").value = image.anchor.x;
+    document.getElementById("anchor_y").value = image.anchor.y;
+    document.getElementById("angle").value = image.angle;
+}
+BasicGame.Game.prototype.toggleAtlasInput = function() {
+    this.buttons.children.forEach((image) => {
+        image.inputEnabled = !image.inputEnabled;
+    });
+    this.toggleUIInput();
+}
+BasicGame.Game.prototype.toggleUIInput = function () {
+    this.ui.children.forEach((image) => {
+        image.inputEnabled = !image.inputEnabled;
+    });
+}
+BasicGame.Game.prototype.configureScaleButtons = function() {
+    scaleUI.ne.anchor.set(1, 0);
+    scaleUI.e.anchor.set(1, 0.5);
+    scaleUI.se.anchor.set(1, 1);
+    scaleUI.s.anchor.set(0.5, 1);
+    scaleUI.sw.anchor.set(0, 1);
+    scaleUI.w.anchor.set(0, 0.5);
+    scaleUI.n.anchor.set(0.5, 0);
+    // north input
+
+    scaleUI.n.events.onInputDown.add((image) => {
+        if (currentImage.anchor.y != 1) {
+            currentImage.anchor.y = 1;
+            currentImage.y += currentImage.height;
+        }
+    });
+    
+    scaleUI.n.events.onDragUpdate.add((image) => {
+        image.x = currentImage.x + currentImage.width * (0.5 - currentImage.anchor.x);
+        currentImage.scale.y = Math.abs(image.y - currentImage.y) / currentImage.options.height;
+        updateUIPosition();
+        updateOptions(currentImage);
+    }, this);
+    // east input
+
+    scaleUI.e.events.onInputDown.add((image) => {
+        if (currentImage.anchor.x != 0) {
+            currentImage.anchor.x = 0;
+            currentImage.x -= currentImage.width;
+        }
+    });
+    scaleUI.e.events.onDragUpdate.add((image) => {
+        image.y = currentImage.y + currentImage.height * (0.5 - currentImage.anchor.y);
+        currentImage.scale.x = Math.abs(image.x - currentImage.x) / currentImage.options.width;
+        updateUIPosition();
+        updateOptions(currentImage);
+    }, this);
+    scaleUI.w.events.onInputDown.add((image) => {
+        if (currentImage.anchor.x != 1) {
+            currentImage.anchor.x = 1;
+            currentImage.x += currentImage.width;
+        }
+    });
+    scaleUI.w.events.onDragUpdate.add((image) => {
+        image.y = currentImage.y + currentImage.height * (0.5 - currentImage.anchor.y);
+        currentImage.scale.x = Math.abs(image.x - currentImage.x) / currentImage.options.width;
+        updateUIPosition();
+        updateOptions(currentImage);
+    }, this);
+    // south input 
+
+    scaleUI.s.events.onInputDown.add((image) => {
+        if (currentImage.anchor.y != 0) {
+            currentImage.anchor.y = 0;
+            currentImage.y -= currentImage.height;
+        }
+    });
+    scaleUI.s.events.onDragUpdate.add((image) => {
+        image.x = currentImage.x + currentImage.width * (0.5 - currentImage.anchor.x);
+        currentImage.scale.y = Math.abs(image.y - currentImage.y) / currentImage.options.height;
+        updateUIPosition();
+        updateOptions(currentImage);
+    }, this);
+    // ne input
+    scaleUI.ne.events.onInputDown.add((image) => {
+        if (currentImage.anchor.y != 1) {
+            currentImage.anchor.y = 1;
+            currentImage.y += currentImage.height;
+        }
+        if (currentImage.anchor.x != 0) {
+            currentImage.anchor.x = 0;
+            currentImage.x -= currentImage.width;
+        }
+    });
+    scaleUI.ne.events.onDragUpdate.add((image) => {
+        currentImage.scale.x = Math.abs(image.x - currentImage.x) / currentImage.options.width;
+        currentImage.scale.y = Math.abs(image.y - currentImage.y) / currentImage.options.height;
+        updateUIPosition();
+        updateOptions(currentImage);
+    }, this);
+    // nw input 
+    scaleUI.nw.events.onInputDown.add((image) => {
+        if (currentImage.anchor.y != 1) {
+            currentImage.anchor.y = 1;
+            currentImage.y += currentImage.height;
+        }
+        if (currentImage.anchor.x != 1) {
+            currentImage.anchor.x = 1;
+            currentImage.x += currentImage.width;
+        }
+    });
+    scaleUI.nw.events.onDragUpdate.add((image) => {
+        currentImage.scale.x = Math.abs(image.x - currentImage.x) / currentImage.options.width;
+        currentImage.scale.y = Math.abs(image.y - currentImage.y) / currentImage.options.height;
+        updateUIPosition();
+        updateOptions(currentImage);
+    }, this);
+    // sw
+    scaleUI.sw.events.onInputDown.add((image) => {
+        if (currentImage.anchor.y != 0) {
+            currentImage.anchor.y = 0;
+            currentImage.y -= currentImage.height;
+        }
+        if (currentImage.anchor.x != 1) {
+            currentImage.anchor.x = 1;
+            currentImage.x += currentImage.width;
+        }
+    });
+    scaleUI.sw.events.onDragUpdate.add((image) => {
+        currentImage.scale.x = Math.abs(image.x - currentImage.x) / currentImage.options.width;
+        currentImage.scale.y = Math.abs(image.y - currentImage.y) / currentImage.options.height;
+        updateUIPosition();
+        updateOptions(currentImage);
+    }, this);
+    // se
+    scaleUI.se.events.onInputDown.add((image) => {
+        if (currentImage.anchor.y != 0) {
+            currentImage.anchor.y = 0;
+            currentImage.y -= currentImage.height;
+        }
+        if (currentImage.anchor.x != 0) {
+            currentImage.anchor.x = 0;
+            currentImage.x -= currentImage.width;
+        }
+    }, this);
+    scaleUI.se.events.onDragUpdate.add((image) => {
+        currentImage.scale.x = Math.abs(image.x - currentImage.x) / currentImage.options.width;
+        currentImage.scale.y = Math.abs(image.y - currentImage.y) / currentImage.options.height;
+        updateUIPosition();
+        updateOptions(currentImage);
+    }, this);
+
+}
---
+++ js/main.js
@@ -0,0 +1,127 @@
+var game;
+var showMenu = false;
+var currentImage = null;
+var imageOptions;
+var command = null;
+var canvas;
+var centerPointer;
+// scale
+var scaleUI = {
+	nw: null,
+	ne: null,
+	sw: null,
+	se: null,
+	s: null,
+	w: null,
+	e: null,
+	n: null
+};
+(function () {
+	console.clear();
+	//	Create your Phaser game and inject it into the game div.
+	//	We did it in a window.onload event, but you can do it anywhere (requireJS load, anonymous function, jQuery dom ready, - whatever floats your boat)
+	//	We're using a game size of 1024 x 768 here, but you can use whatever you feel makes sense for your game of course.
+	game = new Phaser.Game(720, 1280, Phaser.AUTO, 'game', null);
+
+	//	Add the States your game has.
+	//	You don't have to do this in the html, it could be done in your Boot state too, but for simplicity I'll keep it here.
+	game.state.add('Boot', BasicGame.Boot, true);
+	game.state.add('Preloader', BasicGame.Preloader, false);
+	game.state.add('MainMenu', BasicGame.MainMenu, false);
+	game.state.add('Game', BasicGame.Game, false);
+
+	//	Now start the Boot state.
+	game.state.start('Boot');
+	// add some events
+	window.onload = function() {
+		let b = document.getElementsByClassName('add')[0];
+		canvas = document.getElementsByTagName('canvas')[0];
+
+		imageOptions = document.getElementsByClassName('options')[0];
+		b.addEventListener('click', (ev) => {
+			showMenu = true;
+			currentImage = null;
+		});
+		let save = document.getElementsByClassName('save')[0];
+		save.addEventListener('click', (ev) => {
+			currentImage.options.type = document.getElementById("type").value;
+			currentImage.x = +document.getElementById("pos_x").value;
+			currentImage.y = +document.getElementById("pos_y").value;
+			currentImage.anchor.x = +document.getElementById("anchor_x").value;
+			currentImage.anchor.y = +document.getElementById("anchor_y").value;
+			currentImage.scale.x = +document.getElementById("scale_x").value;
+			currentImage.scale.y = +document.getElementById("scale_y").value;
+			updateUIPosition();
+		});
+		document.addEventListener('keyup', (ev) => {
+			if (currentImage != null) {
+				console.log(ev);
+				switch (ev.keyCode) {
+					case 83:
+						toggleScaleUI();
+					break;
+					case 65:
+						console.log('anchor me');
+					break;
+					case 82:
+						console.log('rotate me');
+					break;
+					default:
+					break;
+				}
+			}
+		});
+	}
+})();
+
+function getRandomElementFromArray(arr) {
+	let id = Math.floor(Math.random() * (arr.length));
+	return arr[id];
+}
+function toggleScaleUI() {
+	let keys = Object.keys(scaleUI);
+	keys.forEach((key) => {
+		if (scaleUI[key].alpha == 1) {
+			scaleUI[key].alpha = 0;
+		} else {
+			scaleUI[key].alpha = 1;
+		}
+		scaleUI[key].inputEnabled = !scaleUI[key].inputEnabled;
+		console.log(key, scaleUI[key].inputEnabled);
+	    scaleUI[key].input.enableDrag(scaleUI[key].inputEnabled);
+	});
+	
+	console.log('hello');
+	updateUIPosition();
+}
+function disableScaleUI() {
+	for(let key in scaleUI) {
+		scaleUI[key].alpha = 0;
+		scaleUI[key].inputEnabled = false;
+	    scaleUI[key].input.enableDrag(false);
+	}
+}
+function updateUIPosition() {
+	scaleUI.ne.x = currentImage.x + currentImage.width * (1 - currentImage.anchor.x);
+	scaleUI.ne.y = currentImage.y - currentImage.height * currentImage.anchor.y;
+	scaleUI.nw.x = currentImage.x - currentImage.width * (currentImage.anchor.x);
+	scaleUI.nw.y = currentImage.y - currentImage.height * currentImage.anchor.y;
+
+	scaleUI.sw.x = currentImage.x - currentImage.width * (currentImage.anchor.x);
+	scaleUI.sw.y = currentImage.y + currentImage.height * (1 - currentImage.anchor.y);
+
+	scaleUI.se.x = currentImage.x + currentImage.width * (1 - currentImage.anchor.x);
+	scaleUI.se.y = currentImage.y + currentImage.height * (1 - currentImage.anchor.y);
+
+	scaleUI.n.x = currentImage.x + currentImage.width * (0.5 - currentImage.anchor.x);
+	scaleUI.n.y = currentImage.y - currentImage.height * currentImage.anchor.y;
+
+	scaleUI.s.x = currentImage.x + currentImage.width * (0.5 - currentImage.anchor.x);
+	scaleUI.s.y = currentImage.y + currentImage.height * (1 - currentImage.anchor.y);
+
+	scaleUI.w.x = currentImage.x - currentImage.width * (currentImage.anchor.x);
+	scaleUI.w.y = currentImage.y + currentImage.height * (0.5 - currentImage.anchor.y);
+	
+	scaleUI.e.x = currentImage.x + currentImage.width * (1 - currentImage.anchor.x);
+	scaleUI.e.y = currentImage.y + currentImage.height * (0.5 - currentImage.anchor.y);
+}
